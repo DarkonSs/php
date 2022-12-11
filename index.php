85f71da2d532c122ee05b54fb8fba84f61c75f13
designcore <98830257+DarkonSs@users.noreply.github.com>
2022-12-11 10:01:29 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP TEST</title>
+</head>
+<body>
+    <H1>Does PHP code works?</H1>
+    <br>
+    <br>
+    <?php 
+    echo 'PHP Works correctly'
+    ?>
+</body>
+</html>
